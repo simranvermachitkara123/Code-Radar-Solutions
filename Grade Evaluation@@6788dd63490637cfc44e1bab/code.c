86c9910970878806687e6982e642976ca678f7ef
simranvermachitkara123 <simran0632.becse24@chitkara.edu.in>
2025-02-15 17:49:07 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -1,24 +1,49 @@
-#include <stdio.h>
-int main() {
-    char a;
-    scanf("%c",&a);
-    if(a=='A'){
+// #include <stdio.h>
+// int main() {
+//     char a;
+//     scanf("%c",&a);
+//     if(a=='A'){
+//         printf("Excellent");
+//     }
+//     else if(a=='B'){
+//         printf("Good");
+//     }
+//     else if(a=='C'){
+//         printf("Average");
+//     }
+//     else if(a=='D'){
+//         printf("Below Average");
+//     }
+//     else if(a=='F'){
+//         printf("Fail");
+//     }
+//     else{
+//         printf("Invalid grade");
+//     }
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main(){
+    char c;
+    scanf("%c",&c);
+    switch(c){
+        case 'A':
         printf("Excellent");
-    }
-    else if(a=='B'){
+        break;
+        case 'B':
         printf("Good");
-    }
-    else if(a=='C'){
+        break;
+        case 'C':
         printf("Average");
-    }
-    else if(a=='D'){
-        printf("Below Average");
-    }
-    else if(a=='F'){
+        break;
+        case 'D':
         printf("Fail");
-    }
-    else{
-        printf("Invalid grade");
+        break;
+        default:
+        printf("invalid grade");
+
     }
     return 0;
 }
