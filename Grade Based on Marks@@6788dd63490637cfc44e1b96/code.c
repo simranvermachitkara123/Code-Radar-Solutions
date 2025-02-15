7858da3258d7cc6d886905f602028914adfd2a56
simranvermachitkara123 <simran0632.becse24@chitkara.edu.in>
2025-02-15 15:41:54 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -1,22 +1,43 @@
-#include <stdio.h>
-int main() {
+// #include <stdio.h>
+// int main() {
+//     int m;
+//     scanf("%d",&m);
+//     if (m>=90){
+//         printf("A");
+//     }
+//     else if ((m>=80) && (m<90)){
+//         printf("B");
+//     }
+//     else if ((m>=70) && (m<80)){
+//         printf("C");
+//     }
+//     else if ((m>=60) && (m<70)){
+//         printf("D");
+//     }
+//     else{
+//         printf("F");
+//     }
+   
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main(){
     int m;
     scanf("%d",&m);
     if (m>=90){
         printf("A");
     }
-    else if ((m>=80) && (m<90)){
+    else if((m>=80) && (m<90)){
         printf("B");
     }
-    else if ((m>=70) && (m<80)){
+    else if((m>=70) && (m<80)){
         printf("C");
     }
-    else if ((m>=60) && (m<70)){
+    else if((m>=60) && (m<70)){
         printf("D");
-    }
-    else{
+    }else{
         printf("F");
     }
-   
-    return 0;
 }
