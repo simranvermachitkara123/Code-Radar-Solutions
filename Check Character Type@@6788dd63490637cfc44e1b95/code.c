5a3f41738e06c1fee9705a955e3c5d7c9eea6e52
simranvermachitkara123 <simran0632.becse24@chitkara.edu.in>
2025-02-15 15:35:02 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,18 +1,41 @@
+// #include<stdio.h>
+// #include<ctype.h>
+// int main()
+// {
+//     char c;
+//     scanf("%c",&c);
+//     if(isalpha(c)){
+//         if( c=='A' || c=='a'|| c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' || c=='U' || c=='u'){
+//             printf("Vowel");
+//         }
+//         else{
+//             printf("Consonant");
+//         }
+
+
+//     }
+//     else if (isdigit(c)){
+//         printf("Digit");
+//     }
+//     else{
+//         printf("Special Character");
+//     }
+// }
+
+
+
 #include<stdio.h>
 #include<ctype.h>
-int main()
-{
+int main(){
     char c;
     scanf("%c",&c);
-    if(isalpha(c)){
-        if( c=='A' || c=='a'|| c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' || c=='U' || c=='u'){
-            printf("Vowel");
+    if (isalpha(c)){
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+            print("Vowel");
         }
         else{
             printf("Consonant");
         }
-
-
     }
     else if (isdigit(c)){
         printf("Digit");
@@ -20,4 +43,6 @@ int main()
     else{
         printf("Special Character");
     }
+    return 0;
+
 }
