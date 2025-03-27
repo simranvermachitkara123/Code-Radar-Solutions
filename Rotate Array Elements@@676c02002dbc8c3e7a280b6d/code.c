4f7d96dd60f8e7419703f213b9a26f6e6fc1c28f
simranvermachitkara123 <simran0632.becse24@chitkara.edu.in>
2025-03-27 21:08:35 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,21 +1,37 @@
-// #include<stdio.h>
-// int main()
-// {
-//     int n,k;
-//     scanf("%d",&n);
-//     int arr[n],rotated[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
+// #include <stdio.h>
+
+// void rotateRight(int arr[], int n, int k) {
+//     k = k % n;  // Handle cases where k > n
+
+//     for (int i = 0; i < k; i++) {  // Repeat K times
+//         int last = arr[n - 1];  // Store last element
+
+//         for (int j = n - 1; j > 0; j--) {
+//             arr[j] = arr[j - 1];  // Move each element right
+//         }
+
+//         arr[0] = last;  // Place last element at first
 //     }
-//     scanf("%d",&k);
-//     k=k%n;
-//     for(int i=0;i<n;i++){
-//         int new= (k+i)%n;
-//         rotated[new]=arr[i];
+// }
+
+// int main() {
+//     int n, k;
+//     scanf("%d", &n);  // Read size of array
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);  // Read array elements
 //     }
-//     for(int i=0;i<n;i++){
-//         printf("%d\n",rotated[i]);
+
+//     scanf("%d", &k);  // Read K
+
+//     rotateRight(arr, n, k);  // Rotate array
+
+//     for (int i = 0; i < n; i++) {
+//         printf("%d\n", arr[i]);  // Print rotated array
 //     }
+
+//     return 0;
 // }
 
 
@@ -23,54 +39,29 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include <stdio.h>
-
-void rotateRight(int arr[], int n, int k) {
-    k = k % n;  // Handle cases where k > n
-
-    for (int i = 0; i < k; i++) {  // Repeat K times
-        int last = arr[n - 1];  // Store last element
-
-        for (int j = n - 1; j > 0; j--) {
-            arr[j] = arr[j - 1];  // Move each element right
+#include<stdio.h>
+void rotate(int arr[],int n,int k){
+    k=k%n;
+    for(int i=0;i<k;i++){
+        int last = arr[n-1];
+        for(int j=n-1;j>0;j--){
+            arr[j]=arr[j+1];
         }
-
-        arr[0] = last;  // Place last element at first
+        int arr[0]= last;
     }
 }
-
-int main() {
-    int n, k;
-    scanf("%d", &n);  // Read size of array
+int main(){
+    int n;
+    scanf("%d",&n);
     int arr[n];
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);  // Read array elements
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int k;
+    scanf("%d",&k);
+    rotate(arr,n,k);
+    for(int i=0;i<n;i++){
+        printf("%d\n",arr[i]);
     }
 
-    scanf("%d", &k);  // Read K
-
-    rotateRight(arr, n, k);  // Rotate array
-
-    for (int i = 0; i < n; i++) {
-        printf("%d\n", arr[i]);  // Print rotated array
-    }
-
-    return 0;
 }
