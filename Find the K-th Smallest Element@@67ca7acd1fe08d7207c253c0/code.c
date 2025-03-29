1c790919007a398d7e1f5d85f63207040b4d5193
simranvermachitkara123 <simran0632.becse24@chitkara.edu.in>
2025-03-29 18:05:18 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -1,22 +1,52 @@
-void selectionSort(int arr[] , int n){
-    for(int i=0 ; i<n-1 ;i++){
-        int minindex = i ;
-        for(int j=i+1 ; j<n ; j ++){
-            if(arr[j] < arr[minindex]){
+// void selectionSort(int arr[] , int n){
+//     for(int i=0 ; i<n-1 ;i++){
+//         int minindex = i ;
+//         for(int j=i+1 ; j<n ; j ++){
+//             if(arr[j] < arr[minindex]){
+//                 minindex = j;
+//             }
+//         }
+//         int temp = arr[i];
+//         arr[i]=arr[minindex];
+//         arr[minindex] = temp;
+//     }
+// }
+// int kthSmallest(int arr[],int n,int k){
+//     if(k<1 || k>n){
+//         return -1;
+//     }
+//     selectionSort(arr,n); // Correct function call
+//     return arr[k - 1];  // Return the k-th smallest element
+        
+    
+// }    
+
+
+
+
+
+
+
+
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int minindex = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minindex]){
                 minindex = j;
             }
         }
         int temp = arr[i];
         arr[i]=arr[minindex];
-        arr[minindex] = temp;
+        arr[minindex]=temp;
     }
 }
+
+
 int kthSmallest(int arr[],int n,int k){
-    if(k<1 || k>n){
+    if(k<1||k>n){
         return -1;
     }
-    selectionSort(arr,n); // Correct function call
-    return arr[k - 1];  // Return the k-th smallest element
-        
-    
+    selectionSort(arr,n);
+    return arr[k-1];
 }
