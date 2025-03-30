0d61cc8bb30bbbc845e41afb0f6aa8f5cb2abf44
simranvermachitkara123 <simran0632.becse24@chitkara.edu.in>
2025-03-30 07:47:48 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -39,17 +39,45 @@
 
 
 
+// #include<stdio.h>
+// void rotate(int arr[],int n,int k){
+//     k=k%n;
+//     for(int i=0;i<k;i++){
+//         int last = arr[n-1];
+//         for(int j=n-1;j>0;j--){
+//             arr[j]=arr[j-1];
+//         }
+//         arr[0]= last;
+//     }
+   
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     int k;
+//     scanf("%d",&k);
+//     rotate(arr,n,k);
+//     for(int i=0;i<n;i++){
+//         printf("%d\n",arr[i]);
+//     }
+
+// }
+
 #include<stdio.h>
-void rotate(int arr[],int n,int k){
+
+void rotateArray(int arr[],int n,int k){
     k=k%n;
     for(int i=0;i<k;i++){
-        int last = arr[n-1];
+        int last=arr[n-1];
         for(int j=n-1;j>0;j--){
             arr[j]=arr[j-1];
         }
-        arr[0]= last;
+        arr[0]=last;
     }
-   
 }
 int main(){
     int n;
@@ -60,9 +88,8 @@ int main(){
     }
     int k;
     scanf("%d",&k);
-    rotate(arr,n,k);
+    rotateArray(arr,n,k);
     for(int i=0;i<n;i++){
         printf("%d\n",arr[i]);
     }
-
 }
